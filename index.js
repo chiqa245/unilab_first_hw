5265dc520ab5d627d5c964f06c85e187d397e475
chiqa <mr.avtomaisuradze@gmail.com>
2023-04-29 06:18:16 -0700

    add cards

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 fetch('https://fakerapi.it/api/v1/books')
     .then(response => response.json())
-    .then(r => console.log(r))
     .then(r => {
-        console.log(r.data)
         const bookList = document.getElementById('book-list');
 
         for (i = 0; i < 6; i++) {
+            console.log(r.data[i])
             const li = document.createElement('li');
-            li.textContent = `${r.data[i].title} by ${r.data[i].author}`;
+            const img=document.createElement("img");
+            img.setAttribute("src", r.data[i].image);
+
+            const title=document.createElement("h3");
+            title.textContent = r.data[i].title;
+            const p=document.createElement("p");
+            p.textContent=r.data[i].description;
+            p.setAttribute("class", "color_secondary");
+
             bookList.appendChild(li);
+            li.appendChild(img);
+            li.appendChild(title);
+            li.appendChild(p);
         }
     })
     .catch(error => console.error(error));
