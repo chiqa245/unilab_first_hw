15d5900ccac2be4ce873ed99449bbad8741850ec
chiqa <mr.avtomaisuradze@gmail.com>
2023-05-07 11:51:22 -0700

    add modal

--- index.js
+++ index.js
@@ -9,7 +9,39 @@ function retrieveServicesData(){
             const img=document.createElement("img");
             img.setAttribute("src", r.data[i].image);
 
-            const title=document.createElement("h3");
+            const title=document.createElement("a");
+            title.href="javascript:void(0)";
+            title.style.textDecoration = "none";
+            title.id = i;
+            title.onclick = function jsFunc() {
+                const modal = document.getElementById("modal");
+                modal.style.display = "flex";
+                const modalWrapper = document.getElementById("modal_wrapper");
+                modalWrapper.style.display = "flex";
+                const id = title.id;
+
+                const modalTitle = document.createElement("h3")
+                modalTitle.textContent = r.data[id].title;
+                
+                modalImage = document.createElement("img");
+                modalImage.setAttribute("src", r.data[id].image);
+
+                modalDescription = document.createElement("p");
+                modalDescription.textContent = r.data[id].description;
+
+                modalExit = document.createElement("a");
+                modalExit.textContent = "x";
+                modalExit.onclick =  function close() {
+                    modal.style.display = "none";
+                    modalWrapper.style.display = "none";
+                    modal.innerHTML = '';
+                }
+
+                modal.appendChild(modalExit);
+                modal.appendChild(modalTitle);
+                modal.appendChild(modalImage);
+                modal.appendChild(modalDescription);
+            }
             title.textContent = r.data[i].title;
             const p=document.createElement("p");
             p.textContent=r.data[i].description;
@@ -34,5 +66,7 @@ function retrieveServicesData(){
          x.style.display = "flex";
        }
     }
+
+
 
     retrieveServicesData();
